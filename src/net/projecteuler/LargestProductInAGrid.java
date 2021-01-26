02ed120667b792db0451cc79762d14dd58dde4c1
Kubek <kubol2@gmail.com>
2021-01-26 19:45:52 +0100

    Task #11 First commit

---
+++ src/net/projecteuler/LargestProductInAGrid.java
@@ -0,0 +1,108 @@
+package net.projecteuler;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LargestProductInAGrid {
+
+    public static void main(String[] args) {
+        String declaredMatrix = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n" +
+                "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00\n" +
+                "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\n" +
+                "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91\n" +
+                "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\n" +
+                "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50\n" +
+                "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70\n" +
+                "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21\n" +
+                "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72\n" +
+                "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95\n" +
+                "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92\n" +
+                "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57\n" +
+                "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58\n" +
+                "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40\n" +
+                "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66\n" +
+                "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69\n" +
+                "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36\n" +
+                "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16\n" +
+                "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54\n" +
+                "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+
+        int stringLength = 4;
+        int matrixLength = 20;
+
+        List<List<Integer>> convertedListOfNumbers = new ArrayList<>();
+        Scanner in = new Scanner(declaredMatrix);
+
+        while (in.hasNextLine()) {
+            convertedListOfNumbers.add(getValuesFromLineSeparatedBySpace(in.nextLine()));
+        }
+
+        System.out.println(getBiggestWynikLiczbyObok(stringLength, matrixLength, convertedListOfNumbers));
+        System.out.println(getBiggestWynik(stringLength, matrixLength, convertedListOfNumbers));
+    }
+
+    private static String getBiggestWynikLiczbyObok(int stringLength, int matrixLength, List<List<Integer>> records) {
+        int actualResultSide = 1;
+        int actualResultVertical = 1;
+        int biggestResultSide = 1;
+        int biggestResultVertical = 1;
+        for (int i = 0; i < matrixLength; i++) {
+            for (int j = 0; j <= matrixLength - stringLength; j++) {
+                actualResultSide = 1;
+                actualResultVertical = 1;
+                for (int k = 0; k < stringLength; k++) {
+                    actualResultSide *= records.get(i).get(j + k);
+                    actualResultVertical *= records.get(j + k).get(i);
+                }
+
+                if (actualResultSide > biggestResultSide) {
+                    biggestResultSide = actualResultSide;
+                }
+
+                if (actualResultVertical > biggestResultVertical) {
+                    biggestResultVertical = actualResultVertical;
+                }
+            }
+        }
+        return "Największa liczba poziomo: " + biggestResultSide + " Największa liczba pionowo: " + biggestResultVertical;
+    }
+
+
+    private static String getBiggestWynik(int stringLength, int matrixLength, List<List<Integer>> records) {
+        int actualDiagonalResult = 1;
+        int biggestDiagonalResult = 1;
+        int z = 1;
+        int l = 1;
+        for (int i = 0; i <= matrixLength - stringLength; i++) {
+            for (int j = 0; j <= matrixLength - stringLength; j++) {
+                actualDiagonalResult = 1;
+
+                for (int k = 0; k < stringLength; k++) {
+                    actualDiagonalResult *= records.get(i + k).get(j + k);
+
+                    if (actualDiagonalResult > biggestDiagonalResult) {
+                        biggestDiagonalResult = actualDiagonalResult;
+                    }
+                }
+            }
+        }
+        return "Maksymalna wartosc 4-cyfrowej przekatniej: " + biggestDiagonalResult;
+    }
+
+    private static List<Integer> getValuesFromLineSeparatedBySpace(String givenCodeLine) {
+        List<Integer> readValuesAsList = new ArrayList<Integer>();
+
+        Scanner scannerCodeLine = new Scanner(givenCodeLine);
+        scannerCodeLine.useDelimiter(" ");
+
+        while (scannerCodeLine.hasNext()) {
+            readValuesAsList.add(scannerCodeLine.nextInt());
+        }
+
+        return readValuesAsList;
+    }
+
+
+}
